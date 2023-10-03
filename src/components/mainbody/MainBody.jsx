e1953f1d61e8a2ca655abc902dd20f41b57f9491
jimish1806 <jimish957@gmail.com>
2023-10-04 04:07:54 +0530

    created load more functionality

--- src/components/mainbody/MainBody.jsx
+++ src/components/mainbody/MainBody.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './mainBody.css'
 import PropertyCard from '../propertycard/PropertyCard'
 import propertyData from '../../data'
+import {CgSandClock} from 'react-icons/cg'
 
 const MainBody = () => {
+
+  const initialCards = propertyData.slice(0,6);
+  const [cards , setCards] = useState(initialCards);
+  const [isCompleted , setIsCompleted] = useState(false);
+  const [index , setIndex] = useState(6);
+
+  const loadMore = () => {
+    const newCards = propertyData.slice(index , index + 3);
+    setIndex(index + 3);
+    setCards([...cards , ...newCards]);
+    if(index >= propertyData.length){
+      setIsCompleted(true);
+    }
+  }
+
   return (
     <main className='mainSection'>
         <div className="filterButtons">
@@ -19,12 +35,19 @@ const MainBody = () => {
 
           
             {
-              propertyData.map((property) => {
+              cards.map((property) => {
                 return (                    
                         <PropertyCard key={property.id} {...property} />                    
                 )
               })
             }
+
+        </div>
+        {/* load more button */}
+        <div className='loadmoreSection'>
+        {
+          !isCompleted && <button onClick={loadMore}> <CgSandClock/> Show More...</button>
+        }
         </div>
     </main>
   )
--- src/components/propertycard/PropertyCard.jsx
+++ src/components/propertycard/PropertyCard.jsx
@@ -11,7 +11,7 @@ const PropertyCard = ({id,city,title,location,address,room,bed,bath,sft,price,im
   return (
     <div className='propertyCardDiv'>
         
-        {/* <img src="https://images.unsplash.com/photo-1600047509807-ba8f99d2cdde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1984&q=80" alt=""/> */}
+        
         <div className="imgDiv" style={{backgroundImage : `url(${img})`}}>
             <div>{title}</div>
             <div><FiHeart className='likeIcon'/></div>
